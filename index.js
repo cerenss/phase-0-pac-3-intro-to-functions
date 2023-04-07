be02195876985445b9b7d1d49bf40ac5cbc366a5
Ceren Şahin <cerennssahinn@gmail.com>
2023-04-07 15:18:38 +0300

    Done with practice

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Follow along with the examples here
+function doNothing() {}
+
+function sayHello() {
+    console.log("Hello!");
+}
+
+sayHello();
+
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+}
+
+sayHelloToGuadalupe();
+
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}`);
+}
+
+sayHelloTo("sucre");
+
+function say(greeting, firstName) {
+   return `${greeting}, ${firstName}!`;
+   console.log("I was called");
+}
+say("Hola", "Ceronimo");
+
+function add(x, y) {
+    return x+y;
+}
+
+console.log(add(1, 2));
